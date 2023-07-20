446be4eadf67a6cc860d8fa587fdcf047528e361
Alexander <alshmex209@gmail.com>
2023-07-20 00:28:04 -0500

    UPLOADING BACKEND

---
+++ Atm-project/src/test/java/ATMMachine/Atmproject/AtmProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ATMMachine.Atmproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
